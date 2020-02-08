6a0197c5bf8b395d64d9ecbb35edcc2dec6e077f
Surya Uppuluri <usuryasatyakumar@gmail.com>
2020-02-08 14:59:14 +0530

    DecryptStringFromAlphabets

--- src/main/java/surya/practice/geeks/strings/DecryptStringFromAlphabets.java
+++ src/main/java/surya/practice/geeks/strings/DecryptStringFromAlphabets.java
@@ -65,9 +65,9 @@ public class DecryptStringFromAlphabets {
         int size = s.length();
         for (int i = 0; i < size; i++) {
             if (i + 2 < size && s.charAt(i + 2) == '#') {
-                System.out.println("Substr of " + i + " " + (i + 1) + " is " + s.substring(i, i + 1));
+              /*  System.out.println("Substr of " + i + " " + (i + 1) + " is " + s.substring(i, i + 1));
                 System.out.println("Substr of " + i + " " + (i + 2) + " is " + s.substring(i, i + 2));
-
+*/
                 sb.append((char) ((Integer.parseInt(s.substring(i, i + 2)) - 1) + 'a'));
                 i = i + 2;
             } else sb.append((char) ((Integer.parseInt(String.valueOf(s.charAt(i))) - 1) + 'a'));
